6a249bd33717ce0cd78e413aaddf56522bb2a3fa
asen <asen.borisov7@yahoo.com>
2022-11-30 12:53:19 +0200

    game() added

--- main.py
+++ main.py
@@ -1,5 +1,59 @@
-#game code
+from data import data
+import random
+from art import logo, vs
+import os
 
-from art import logo
 
-print(logo)
+def get_random():
+    return random.choice(data)
+
+
+def format_data(account):
+    name = account["name"]
+    description = account["description"]
+    country = account["country"]
+    # print(f'{name}: {account["follower_count"]}')
+    return f"{name}, a {description}, from {country}"
+
+
+def check_answer(guess, a_followers, b_followers):
+    if a_followers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
+
+
+def game():
+    print(logo)
+    score = 0
+    game_should_continue = True
+    account_a = get_random()
+    account_b = get_random()
+
+    while game_should_continue:
+        account_a = account_b
+        account_b = get_random()
+
+        while account_a == account_b:
+            account_b = get_random()
+
+        print(f"Compare A: {format_data(account_a)}.")
+        print(vs)
+        print(f"Against B: {format_data(account_b)}.")
+
+        guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+        a_follower_count = account_a["follower_count"]
+        b_follower_count = account_b["follower_count"]
+        is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+        os.system('cls')
+        print(logo)
+        if is_correct:
+            score += 1
+            print(f"You're right! Current score: {score}.")
+        else:
+            game_should_continue = False
+            print(f"Sorry, that's wrong. Final score: {score}")
+
+
+game()
